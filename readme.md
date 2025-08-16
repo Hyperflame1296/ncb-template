aa9404a390d37836f0fec4d6cc313c3dece46204
Hyperflame1296 <jasircranshaw001@gmail.com>
2025-08-16 01:16:11 -0400

    various changes

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
-# ncb-template
-template, for making mods using ncb
-this is mostly for myself, and it makes creating mods & stuff easier
+# NCB Template
+A template for making mods using NCB!  
+This is mostly for myself, and it acts as a baseline for making mods.
+
+- Made by [TensiveYT](https://youtube.com/@Hyperflamee8)
+
+## Contributors
+(none yet)
--- scripts/ncb/main.js
+++ scripts/ncb/main.js
@@ -1,89 +1,90 @@
-import * as s  from '@minecraft/server';
-import * as ui from '@minecraft/server-ui';
-import * as gt from '@minecraft/server-gametest';
-import * as cmn from '@minecraft/common';
-import * as dbg from '@minecraft/debug-utilities';
+import * as s from '@minecraft/server'
+import * as ui from '@minecraft/server-ui'
+import * as gt from '@minecraft/server-gametest'
+import * as cmn from '@minecraft/common'
+import * as dbg from '@minecraft/debug-utilities'
 
 /*
-    ncb template v0.1.2.rev0
+    ncb template v0.1.3.rev0
     template for making mods using ncb (new codebase)
 */
 
 let ncb = {
-    ver: 'ncb0.1.2.rev0',
+    ver: 'v0.1.3.rev0',
     methods: {
-        check_op: function(player) { // wrap the operator check, to make things easier
-            if (player.commandPermissionLevel >= 2) return true;
+        isOp: function(player) {
+            // wrap the operator check, to make things easier
+            if (player.commandPermissionLevel >= 2) return true
             return false
+        },
+        rankText: function(player) {
+            return ncb.ranks[player.name] ? ncb.ranks[player.name].map(r => `§i[§${r.colorCode}${r.text}\xa7i]\xa7r`).join(' ') + '\xa7r ' : ''
         }
     },
-    debug: {
-        run_thru: function(v) {
-            return eval(v);
-        }
+    ranks: {
+        // example:
+        /*
+        TensiveYT: [
+            {
+                colorCode: '6',
+                text: 'Owner'
+            },
+            {
+                colorCode: 'a',
+                text: 'Penguin'
+            }
+        ]
+        */
     },
-    listeners: {
-        before_events: {
-            chatSend: function(e) {
-                // 100 max characters
-                if (e.message.length > 100) {
-                    e.sender.sendMessage(`\xa7cYou can\'t send a message that long\xa7f! \xa7f(\xa7c${e.message.length} \xa7f>\xa7c 100\xa7f)`)
-                    return;
-                } 
+    worldListeners: {
+        beforeEvents: {
+            chatSend: function (e) {
+                e.cancel = true
+                // 256 max characters
+                if (e.message.length > 256) {
+                    e.sender.sendMessage(`§cYou can\'t send a message that long§f! §f(§c${e.message.length} §f>§c 256§f)`)
+                    return
+                }
                 // makes it so you can't use §k in chat
-                if (e.message.includes('\xa7k')) {
-                    e.sender.sendMessage(`\xa7cYou can\'t use that formatting code in chat\xa7f!`)
-                    return;
+                if (e.message.includes('§k')) {
+                    e.sender.sendMessage(`§cYou can\'t use that formatting code in chat§f!`)
+                    return
                 }
-                s.world.sendMessage(`${e.sender.name} \xa7i»\xa7r ${e.message}`) // send the message globally
-            }
+                s.world.sendMessage(`${ncb.methods.rankText(e.sender)}${e.sender.name} §i»§r ${e.message}`) // send the message globally
+            },
         },
-        after_events: {
-            playerSpawn: function(e) {
-                // runs when a player spawns
-                if (e.initialSpawn) {
-                    let player = e.player;
-                        player.sendMessage(`\xa7ewelcome to the ncb template\xa7f! \xa7i- \xa7f(\xa7e${ncb.ver}\xa7f)`);
-                }
-            }
-        }
+        afterEvents: {}
     },
-    listeners_system: {
-        before_events: {
-            
-        },
-        after_events: {
-            
-        }
+    systemListeners: {
+        beforeEvents: {},
+        afterEvents: {}
     },
-    on_tick: function() {
-        s.system.runInterval(() => {
-            // runs every game tick
-        })
+    onTick: function () {
+        // runs every game tick
     },
-    on_load: function() {
+    onReload: function () {
         // runs when the script is loaded
-        s.world.sendMessage(`\xa7fScript reloaded\xa7i! (NCB Template)`);
-    }
+        s.world.sendMessage(`§i[§eNCB§i] §fscript reloaded!`)
+    },
 }
 s.world.afterEvents.worldLoad.subscribe(() => {
-    for (let key of Object.keys(ncb.listeners.before_events)) {
-        s.world.beforeEvents[key].subscribe(ncb.listeners.before_events[key]);
-    }
-    for (let key of Object.keys(ncb.listeners.after_events)) {
-        s.world.afterEvents[key].subscribe(ncb.listeners.after_events[key]);
-    }
-    for (let key of Object.keys(ncb.listeners_system.before_events)) {
-        s.system.beforeEvents[key].subscribe(ncb.listeners_system.before_events[key]);
-    }
-    for (let key of Object.keys(ncb.listeners_system.after_events)) {
-        s.system.afterEvents[key].subscribe(ncb.listeners_system.after_events[key]);
-    }
+    for (let key of Object.keys(ncb.worldListeners.beforeEvents))
+        s.world.beforeEvents[key].subscribe(ncb.worldListeners.beforeEvents[key])
+
+    for (let key of Object.keys(ncb.worldListeners.afterEvents))
+        s.world.afterEvents[key].subscribe(ncb.worldListeners.afterEvents[key])
+
+    for (let key of Object.keys(ncb.systemListeners.beforeEvents))
+        s.system.beforeEvents[key].subscribe(ncb.systemListeners.beforeEvents[key])
+
+    for (let key of Object.keys(ncb.systemListeners.afterEvents))
+        s.system.afterEvents[key].subscribe(ncb.systemListeners.afterEvents[key])
+
     ncb.dimensions = {
         overworld: s.world.getDimension('minecraft:overworld'),
         nether: s.world.getDimension('minecraft:nether'),
-        the_end: s.world.getDimension('minecraft:the_end')
+        end: s.world.getDimension('minecraft:the_end'),
     }
-    ncb.on_load();
-    ncb.on_tick();
+    ncb.onReload()
+    s.system.runInterval(ncb.onTick)
 })
